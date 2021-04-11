270b0f0b722b66c1cffb8324211cf442e43a4fde
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-11 20:14:51 +0500

    1

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+Ui_MainWindown, _ = uic.loadUiType('uis/untitled.ui')
+
+# --------константы
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+static_server = 'https://static-maps.yandex.ru/1.x/'
+geocode_key = "40d1649f-0493-4b70-98ba-98533de7710b"
+geocode_server = "https://geocode-maps.yandex.ru/1.x/"
+l = ['map', 'sat', 'sat,skl']
 
 
-# Press the green button in the gutter to run the script.
+# ---------/константы
+
+
+class Dot:
+    def __init__(self, a, b):
+        self.a, self.b = a, b
+
+    def __str__(self):
+        return f'{self.a},{self.b}'
+
+
+class Ui_MainWindow(Ui_MainWindown, QtWidgets.QMainWindow):
+    def init2(self):
+        self.li = 2
+        self.spn = 0.01
+        self.dot = Dot(37.620070, 55.753630)
+        self.get_img()
+
+    def get_img(self):
+        params = {
+            'l': l[self.li],
+            'll': self.dot.__str__(),
+            "spn": f"{self.spn},{self.spn}",
+        }
+        open('img.png', 'wb').write(requests.get(static_server, params=params).content)
+        self.img.setPixmap(QtGui.QPixmap('img.png'))
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    form = Ui_MainWindow()
+    form.setupUi(form)
+    form.init2()
+    form.show()
+    sys.excepthook = except_hook
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sys.exit(app.exec())
