a500d3f905df2d683897b2a8833c0c19171cbc74
Sve4ka <sve4ka_one@mail.ru>
2021-04-20 18:46:31 +0500

    свечка старалась

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 при проверке pgup/pgdn проверьте что не изменяете текст или комбобокс
 '''
 
-import sys
+import sys, math
 
 import requests
 from PyQt5 import QtGui
@@ -35,6 +35,9 @@ class Dot:
     def __str__(self):
         return f'{self.a},{self.b}'
 
+    def __sub__(self, other):
+        print(self, other)
+
     def __iadd__(self, other):
         self.a += other[0]
         self.b += other[1]
@@ -60,7 +63,8 @@ class Ui_MainWindow(Ui_MainWindown, QtWidgets.QMainWindow):
         self.li = 2
         self.dots = []
         self.spn = 0.01
-        self.maindot = Dot(55.0, 54.0)
+        self.z = 15
+        self.maindot = Dot(55.94386499259262, 54.726269546473326)
         self.get_img()
         self.pushButton.clicked.connect(self.get_img)
         self.pushButton_find.clicked.connect(self.find_pos)
@@ -111,10 +115,106 @@ class Ui_MainWindow(Ui_MainWindown, QtWidgets.QMainWindow):
                     'formatted'])
             self.get_img()
 
-    # def mousePressEvent(self, event):
-    #     if (event.button() == Qt.LeftButton):
-    #         pos = (event.x(), event.y())
-    #         if 0<=event[0]-10<=600 and 0<=pos[1]-10<=450:
+    def mousePressEvent(self, event):
+        if (event.button() == Qt.LeftButton):
+            pos = (event.x() - 10, event.y() - 10)
+            if 0 <= pos[0] <= 600 and 0 <= pos[1] <= 450:
+                x = pos[0] - 300
+                y = -pos[1] + 225
+                a, b = self.maindot.a + 360 / (2 ** self.z) * x / 256, self.maindot.b + 180 / (2 ** self.z) * y / 256
+                s = Dot(a, b)
+                self.dots.append(s)
+                self.find_pos()
+                self.dots.pop(-1)
+
+        if (event.button() == Qt.RightButton):
+            pos = (event.x() - 10, event.y() - 10)
+            if 0 <= pos[0] <= 600 and 0 <= pos[1] <= 450:
+                x = pos[0] - 300
+                y = -pos[1] + 225
+                a, b = self.maindot.a + 360 / (2 ** self.z) * x / 256, self.maindot.b + 180 / (
+                                2 ** self.z) * y / 256
+                s = Dot(a, b)
+                self.dots.append(s)
+                self.find_pos()
+            # if self.lonlat_distance() <= 50:
+                search_api_server = "https://search-maps.yandex.ru/v1/"
+                api_key = "..."
+
+                address_ll = str(self.maindot)
+
+                search_params = {
+                    "apikey": api_key,
+                    "lang": "ru_RU",
+                    "ll": address_ll,
+                    "type": "biz"
+                }
+                geocode = str(s)
+                response = requests.get(search_api_server, params=search_params)
+                # Преобразуем ответ в json-объект
+                json_response = response.json()
+
+                geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+                geocoder_params = {
+                    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                    "geocode": geocode,
+                    "format": "json",
+                }
+                print(json_response)
+                organization = response.json()["features"]
+                # Название организации.
+                org_name = organization["properties"]["CompanyMetaData"]["name"]
+                # Адрес организации.
+                org_address = organization["properties"]["CompanyMetaData"]["address"]
+
+                # Получаем координаты ответа.
+                point = organization["geometry"]["coordinates"]
+                org_point = "{0},{1}".format(point[0], point[1])
+                delta = "0.005"
+
+                # Собираем параметры для запроса к StaticMapsAPI:
+                map_params = {
+                    # позиционируем карту центром на наш исходный адрес
+                    "ll": address_ll,
+                    "spn": ",".join([delta, delta]),
+                    "l": "map",
+                    # добавим точку, чтобы указать найденную аптеку
+                    "pt": "{0},pm2dgl".format(org_point)
+                }
+
+                map_api_server = "http://static-maps.yandex.ru/1.x/"
+                # ... и выполняем запрос
+                response = requests.get(map_api_server, params=map_params)
+
+                name = self.dots[-1]["properties"]["CompanyMetaData"]["name"]
+                print(name)
+                self.pt = self.maindot + ',pm2rdl'
+                self.show_message(
+                    msg=name,
+                    style='color: white; background-color: green; '
+                          'font-size: 16pt;')
+                self.update_pixmap()
+            else:
+                print('Близкой организации не найдено')
+
+
+    def lonlat_distance(self):
+        """Расстояние между точками"""
+        degree_to_meters_factor = 111 * 1000  # 111 километров в метрах
+        print(self.dots)
+        a_lon, a_lat = self.dots[-1].a, self.dots[-1].b
+        b_lon, b_lat = self.maindot.a, self.maindot.b
+
+        # Берем среднюю по широте точку и считаем коэффициент для нее.
+        radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+        lat_lon_factor = math.cos(radians_lattitude)
+
+        # Вычисляем смещения в метрах по вертикали и горизонтали.
+        dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+        dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+        return round(math.sqrt(dx * dx + dy * dy), 4)
 
 
     def get_img(self):
@@ -131,9 +231,11 @@ class Ui_MainWindow(Ui_MainWindown, QtWidgets.QMainWindow):
         if event.key() == Qt.Key_PageUp:
             if self.spn * 2 < 2:
                 self.spn *= 2
+                self.z -= 1
         elif event.key() == Qt.Key_PageDown:
             if self.spn / 2 > 0.00175:
                 self.spn /= 2
+                self.z += 1
         elif event.key() == Qt.Key_Left:
             self.maindot += [-self.spn * 2, 0]
         elif event.key() == Qt.Key_Right:
